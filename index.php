397fb95be7035491cd259e3a5538f431cd6d0f15
Pablo Muralles <pablomuralles28@gmail.com>
2022-11-04 17:45:14 -0600

    the index redirect to the page list php

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
-    <title>Inscripciones Eventos CDAG</title>
-</head>
-<body>
-    <center> <h1>Eventos CDAG<h1></center>
-    <p>Bienvenidos al sistema de la CDAG para el control de los eventos impartidos</p>
-    <p>Selecciones la opción que desea:</p>
+<?php
+    include(__DIR__ . "/events/list.php");
 
-    <input type="button" value="Crear Evento" class="create_event_button"  id="btn_create_event" onClick="document.location.href='events/create.php'" />
-
-    <!-- <input type="button" value="Eliminar Evento" class="delete_event_button" id="btn_create_event" onClick="document.location.href='events/delete.p'" />
-   
-    <input type="button" value="Modificar Evento" class="modify_event_button" id="btn_create_event" onClick="document.location.href=' '" /> -->
-
-    <input type="button" value="Lista de evento" class="list_event_button" id="btn_create_event" onClick="document.location.href='events/list.php'" />
-
-
-
-</body>
-</html>
+    header("Location: events/list.php");
+?>
 
  
