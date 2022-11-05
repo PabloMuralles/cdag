f8979a4856ff3b9ebede901f9c3012b269fd223b
Pablo Muralles <pablomuralles28@gmail.com>
2022-11-04 23:38:20 -0600

    chages dearly - view list to show the button crear evento

--- events/list.php
+++ events/list.php
@@ -9,38 +9,42 @@ $result = $mysqli->query($sql);
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <title>Eventos</title>
-    </head>
-    <body>
 
-        <div class="container pt-3">
-            <h1 class="text-center">Eventos</h1>
-        </div>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>Eventos</title>
+</head>
 
-        <div class="container pt-3 nav justify-content-end">
-        <a href='../events/create.php' class='btn btn-primary '>Crear Evento</a>
+<body>
+    <table style="width: 100%;">
+        <tr>
+            <th style="width: 30%;"></th>
+            <th style="width: 40%;">
+                <h1 class="text-center">Eventos</h1>
+            </th>
+            <th style="width: 30%; margin-bottom: 0;">
+                <div class="text-center"><a class="btn btn-primary " href='../events/create.php'>Crear Evento</a>
+                </div>
+            </th>
+        </tr>
+    </table>
 
-        </div>
+    <div class="container mt-3">
+        <?php
+        if ($result->num_rows > 0) {
+            $events = $result->fetch_all(MYSQLI_ASSOC);
 
+            echo "<ul class='list-group list-group-flush'>";
 
-        <div class="container mt-3">
-            <?php
-                if ($result->num_rows > 0) {
-                    $events = $result->fetch_all(MYSQLI_ASSOC);
+            foreach ($events as $event) {
+                $id = $event['id'];
+                $name = $event['nombre'];
 
-                    echo "<ul class='list-group list-group-flush'>";
-
-                    foreach ($events as $event) {
-                        $id = $event['id'];
-                        $name = $event['nombre'];
-
-                        echo
-                        "
+                echo
+                "
                         <li class='list-group-item d-flex justify-content-between align-items-center'>
                             <p>" . $name . "</p>
                             <div class='btn-group'>
@@ -50,13 +54,14 @@ $result = $mysqli->query($sql);
                             </div>
                         </li>
                         ";
-                    }
-                    echo "</ul>";
-                } else {
-                    echo "<p class='text-center fs-3'>No existe ningun evento</p>";
-                }
-            ?>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    </body>
+            }
+            echo "</ul>";
+        } else {
+            echo "<p class='text-center fs-3'>No existe ningun evento</p>";
+        }
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
 </html>
